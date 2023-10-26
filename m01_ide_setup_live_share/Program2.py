e926249be25e4af085cd3ef8cb9d195f47510392
Wintxr <108226679+wintxrcall@users.noreply.github.com>
2023-10-26 10:14:55 -0400

    M1-Variable_p1
    This will calculate a users age based off the current year and the birth year they input.

---
+++ m01_ide_setup_live_share/Program2.py
@@ -0,0 +1,28 @@
+# def count_words(text):
+#     words = text.split()
+#     return len(words)
+
+# def average_word_length(text):
+#     words = text.split()
+#     total_characters = sum(len(word) for word in words)
+#     word_count = count_words(text)
+#     return total_characters / word_count if word_count > 0 else 0
+
+    # try:
+    #     text = input("Enter a string of text: ")
+    #     if not text:
+    #         raise ValueError("Error: Text cannot be empty.")
+
+# num_words = count_words(text)
+# avg_word_length = average_word_length(text)
+
+# print(f"Word count: {num_words}")
+# print(f"Average word length: {avg_word_length: .2f}")
+
+# except ValueError as e:
+#     print(e)
+
+def main():
+    print('test')
+
+main()
