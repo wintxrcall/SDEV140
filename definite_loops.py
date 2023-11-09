64e86f84dd9ad6b4c0b4efe324240eafdeebad23
Wintxr <108226679+wintxrcall@users.noreply.github.com>
2023-11-08 19:19:52 -0500

    M03 Definite Loops

---
+++ definite_loops.py
@@ -0,0 +1,37 @@
+# Author Renee Guldi	
+# SDEV140 MPJovanovich
+# This Python program is to be used to build a number pyramid
+# based on a given height (integer)
+
+    ## Prompt the user for input and return it as the correct data type.
+def get_input() -> int:
+        height = int(input("Enter the height of your pyramid: "))
+        return height
+
+def write_pyramid_row(row: int, height: int) -> None:
+  
+    # Print spaces before numbers
+    for _ in range(height - row):
+        print(" ", end=" ")
+
+    # Print increasing numbers
+    for num in range (1, row + 1):
+         print(num, end=" ")
+    # Print decreasing numbers (if there are any)
+    for num in range (row - 1, 0, -1):
+         print(num, end=" ")
+    
+    # this will push the next pyramid row to the next line
+    print("\n")
+
+
+def create_pyramid(height: int) -> None:
+    # Loop through write_pyramid_row to create the full pyramid
+    for row in range(1, height + 1):
+        write_pyramid_row(row, height)
+        
+
+
+if __name__ == "__main__":
+    height = get_input()
+    create_pyramid(height)
